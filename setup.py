becacb5588cfd342d61111a889060754255c2ce4
ahothan <ahothan@cisco.com>
2015-08-12 16:54:59 -0700

    Add pbr to handle versioning, update README
    Change-Id: I5163000a623a7e66a274e767560f6f285b367a97

--- setup.py
+++ setup.py
@@ -18,21 +18,9 @@ class Tox(test):
         import tox
         sys.exit(tox.cmdline())
 
-__version__ = '0.1.1'
-
 if __name__ == '__main__':
-    setup(
-        name='hdrhistogram',
-        version=__version__,
-        description='High Dynamic Range histogram in native python',
-        url='https://github.com/ahothan/hdrhistogram',
-        download_url='https://github.com/ahothan/hdrhistogram/tarball/' + __version__,
-        author='Alec Hothan',
-        author_email='ahothan@gmail.com',
-        license='Apache 2.0',
-
-        packages=['hdrh'],
-        keywords='hdrhistogram hdr histogram high dynamic range',
-        tests_require=['tox'],
-        cmdclass={'test': Tox}
+    setup(setup_requires=['pbr'], pbr=True,
+          keywords='hdrhistogram hdr histogram high dynamic range',
+          tests_require=['tox'],
+         cmdclass={'test': Tox}
     )
--- test/test_hdrhistogram.py
+++ test/test_hdrhistogram.py
@@ -6,8 +6,18 @@ https://github.com/HdrHistogram/HdrHistogram (Java)
 https://github.com/HdrHistogram/HdrHistogram_c (C)
 
 Written by Alec Hothan
-Apache License 2.0
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 '''
 
 import datetime
@@ -744,9 +754,6 @@ ENCODE_SAMPLE_WRK2_C = [
 def test_hdr_interop_wrk2():
     histogram = HdrHistogram(LOWEST, IMPORTED_MAX_LATENCY, 2)
     histogram.decode_and_add(ENCODE_SAMPLE_WRK2_C[0])
-    perc_dict = histogram.get_percentile_to_value_dict([50, 90, 99, 99.9, 99.99, 99.999])
-    print perc_dict
-
 
 @pytest.mark.perf
 def test_hist_dec_perf():
