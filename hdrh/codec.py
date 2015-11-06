7984bf825bfbac8cc1f3354c1f4beb43a2f0d22e
Nick Johnson <nickj21@gmail.com>
2015-11-06 08:55:11 -0800

    No longer need to enforce unicode

--- hdrh/codec.py
+++ hdrh/codec.py
@@ -22,7 +22,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-from __future__ import print_function, unicode_literals
+from __future__ import print_function
 from builtins import str
 from builtins import range
 from builtins import object
